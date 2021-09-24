2c2681feae69b289b5a99c95edbf5e26921aed3d
malbasini <malbasini@gmail.com>
2021-09-24 10:41:16 +0200

    First Program

---
+++ FirstProgram/Program.py
@@ -0,0 +1,10 @@
+def paridispari():
+    inp = input("Inserisci un Numero")
+    numero = int(inp)
+    modulo = numero % 2
+    if modulo == 0:
+        print("Numero Pari")
+    else:
+        print("Numero Dispari")
+
+paridispari()
