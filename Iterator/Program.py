5aca05431821b7b5909d2faaaa847ae56b02f0d5
malbasini <malbasini@gmail.com>
2021-11-04 12:00:00 +0100

    commit

---
+++ Iterator/Program.py
@@ -0,0 +1,24 @@
+#L'algoritmo itera tra 0 e un valore intero passato come parametro
+class MyIterator():
+    def __iter__(self):
+        self.myAttr = 0
+        return self
+    def __next__(self):
+        if self.myAttr <= self.number:
+            n=self.myAttr
+            self.myAttr+=1
+            return n
+        else:
+            raise StopIteration
+    @property
+    def number(self):
+        return self.__number__
+    @number.setter
+    def number(self,number):
+        self.__number__ = number
+
+m = MyIterator()
+m.number=300
+myIter = iter(m)
+for x in myIter:
+    print(x)
