2774c5877914f0c3fa2799381051f01d8df4e5ba
malbasini <malbasini@gmail.com>
2021-11-07 23:39:57 +0100

    Commit

--- DataAnnotations/Program.py
+++ DataAnnotations/Program.py
@@ -1,12 +1,12 @@
 from typing import List
-#Diamo un'occhiata a un paio di esempi. I tre esempi seguenti 
-#accettano due parametri e restituiscono la loro somma. 
-#Come puoi vedere, <expression> può essere qualsiasi cosa. 
-#Tutte e tre sono definizioni di funzioni valide ma mostra 
-#diversi modi di utilizzare le annotazioni di funzione.
-#Nota che se ci sono parametri con valori predefiniti, 
-#le annotazioni devono sempre precedere il valore predefinito 
-#di un parametro come hai visto nel terzo esempio sotto riportato.
+#Let's look at a couple of examples. The following three examples
+# take two parameters and return their sum.
+# As you can see, <expression> can be anything.
+#All three are valid function definitions but it shows
+#different ways to use function annotations.
+#Note that if there are parameters with default values,
+#notations must always precede the default value
+#of a parameter as you saw in the third example below.
 def func1(num1:str = "1st param", num2:str = "2nd param") -> str:
     return num1 + num2
 def func2(num1: int, num2: int) -> int:
@@ -24,14 +24,14 @@ name: str
 city: str = 'Mysor'
 age: int = 35
 
-# Nell'esempio seguente, la funzione quadrato si aspetta 
-# un parametro intero num e restituisce i quadrati di tutti 
-# i numeri da 0 a num. La variabile squares è dichiarata come 
-# List [int] a indicare che contiene un elenco di numeri interi. 
-# Allo stesso modo, anche il tipo restituito dalla funzione è List [int] . 
-# Successivamente, square .__ annotations__ fornisce 
-# annotazioni locali alla funzione e __annotations__ fornisce 
-# annotazioni globali. 
+# In the following example, the square function expects
+# an integer parameter num and returns all squares
+# the numbers from 0 to num. The squares variable is declared as
+# List [int] to indicate that it contains a list of integers.
+# Similarly, the return type of the function is also List [int].
+# Next, square .__ annotations__ provides
+# local annotations to the function and __annotations__ provides
+# global annotations.
 
 
 squares: List[int] = []
@@ -42,7 +42,7 @@ def square(num: int) -> List[int]:
 print(square(10))
 print(square.__annotations__)
 print(__annotations__)
-#Anche le classi possono usare le data Annotations
+#Class can also use data Annotations
 class MyClass:
     nome: str
     cognome:str
--- DataClasses/Program.py
+++ DataClasses/Program.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
-#A partire dalle annotazioni di tipo Python è in grado di inferire
-#la presenza di tipi di oggetto Field che rappresentano i campi della classe
+#Starting from annotations of type Python is able to infer
+#the presence of Field object types that represent the fields of the class
 @dataclass(init=True,repr=True,order=True,frozen=False)
 class MyClass(object):
     nome: str
     cognome: str
 
 mc1=MyClass(nome='Luigi',cognome='Rossi')
-#Il metodo repr facendo una print(mc) ci fornisce una stampa più compatta.
+# The repr method doing a print(mc) gives us a more compact print.
 print(mc1)
 mc2=MyClass(nome='Luigi',cognome='Rossi')
 print(mc2)
