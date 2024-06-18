0cfedc5263bb5af282fc5a3347ef520a45b9a2cd
malbasini <malbasini@gmail.com>
2024-06-18 06:10:41 +0200

    commit

--- ChatGPT/test_api_chat1.py
+++ ChatGPT/test_api_chat1.py
@@ -1,10 +1,21 @@
+import sys
 import openai
 from decouple import config
-openai.api_key=config("OPENAI_API_KEY")
-#Secondo esempio con la classe ChatCompletion
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role":"user","content":"Chi ha sviluppato Python?"}
-    ])
-print(completion.choices[0].message.content)
+openai.api_key=config('OPENAI_API_KEY')
+
+#Terzo esempio una conversazione tra l'utente e l'assistente virtuale
+def chat_with_openai():
+    chat_history=[]
+    while True:
+        user_input=input("User:" )
+        chat_history.append({"role":"user","content":user_input})
+        response=openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=chat_history
+        )
+        assistant_response=response.choices[0].message.get("content")
+        print("Assistant:", assistant_response)
+        chat_history.append({"role": "assistant", "content": assistant_response})
+        if user_input.lower()=="fine":
+            sys.exit()
+chat_with_openai()
--- ChatGPT/test_api_chat2.py
+++ ChatGPT/test_api_chat2.py
@@ -1,21 +1,14 @@
 import sys
 import openai
 from decouple import config
-openai.api_key=config("OPENAI_API_KEY")
-#Terzo esempio una conversazione tra l'utente e l'assistente virtuale
-def chat_with_openai():
-    chat_history=[]
-    while True:
-        user_input=input("User:" )
-        chat_history.append({"role":"user","content":user_input})
-        response=openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=chat_history
-        )
-        assistant_response=response.choices[0].message.get("content")
-        print("Assistant:", assistant_response)
-        chat_history.append({"role": "assistant", "content": assistant_response})
-        if user_input.lower()=="fine":
-            sys.exit()
-chat_with_openai()
+openai.api_key=config('OPENAI_API_KEY')
 
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
